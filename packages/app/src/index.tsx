855e90d7ecbd191e6417d47cc4ffd4a4d10b033d
Yasser Zadeh <yassermzh.germany@gmail.com>
2020-03-27 00:44:52 +0100

    app: use config

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -4,10 +4,10 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import AppNavigator from "./navigation";
-const API_BASE_URL = "http://localhost:8000/graphql";
+import config from './config'
 
 const httpLink = new HttpLink({
-  uri: API_BASE_URL,
+  uri: config.API,
 });
 
 const cache = new InMemoryCache();
