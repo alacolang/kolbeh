ffb19fb047cdf9655d1e4e66fa3cfb51296f2924
Yasser Zadeh <yassermzh.germany@gmail.com>
2020-03-27 23:02:39 +0100

    app: change/add icons

--- packages/app/src/icons/index.ts
+++ packages/app/src/icons/index.ts
@@ -1,5 +1,3 @@
-// fdsdfsf <reference path="../../typings/custom/hello.d.ts" />
-
 import ParentActiveIcon from "./images/parent-active.png";
 import ParentIcon from "./images/parent.png";
 import KidIcon from "./images/kid.png";
@@ -8,7 +6,20 @@ import HomeIcon from "./images/home.png";
 import HomeActiveIcon from "./images/home-active.png";
 import BackIcon from "./images/back.png";
 import ClipIcon from "./images/clip-active.png";
+import LogoIcon from "./images/logo.png";
+import AnxietyIcon from "./images/empathy.png";
+import ArrowIcon from "./images/arrow.png";
 import { ImageSourcePropType } from "react-native";
+import EmpathyIcon from "./images/empathy.png";
+import GriefIcon from "./images/grief.png";
+import StressIcon from "./images/stress.png";
+import DepressionIcon from "./images/depression.png";
+import ObsessionIcon from "./images/obsession.png";
+import EducationIcon from "./images/education.png";
+import AdhdIcon from "./images/adhd.png";
+import AutismIcon from "./images/autism.png";
+import BipolarIcon from "./images/bipolar.png";
+import ParentingIcon from "./images/parenting.png";
 
 type Icons = Record<string, ImageSourcePropType>;
 
@@ -21,6 +32,19 @@ const icons: Icons = {
   homeActive: HomeActiveIcon,
   back: BackIcon,
   clip: ClipIcon,
+  logo: LogoIcon,
+  anxiety: AnxietyIcon,
+  arrow: ArrowIcon,
+  empathy: EmpathyIcon,
+  grief: GriefIcon,
+  stress: StressIcon,
+  depression: DepressionIcon,
+  obsession: ObsessionIcon,
+  education: EducationIcon,
+  adhd: AdhdIcon,
+  autism: AutismIcon,
+  bipolar: BipolarIcon,
+  parenting: ParentingIcon,
 };
 
 export default icons;
