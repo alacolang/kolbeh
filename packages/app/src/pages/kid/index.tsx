29833585b692a0a514b71cdd309a693289551dcb
Yasser Zadeh <yassermzh.germany@gmail.com>
2020-03-28 18:25:46 +0100

    app: reset kid page

--- packages/app/src/pages/kid/index.tsx
+++ packages/app/src/pages/kid/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import Video from "react-native-video";
 import config from "../../config";
 
 // Within your render function, assuming you have a file called
@@ -10,17 +9,8 @@ import config from "../../config";
 const Kid = () => {
   // let ref = React.useRef();
   return (
-    <View style={styles.container}>
-    <Video
-      source={{ uri: config.HOST + "/static/images/grief.mp4" }} // Can be a URL or a local file.
-      // ref={(ref) => {
-      //   this.player = ref;
-      // }} // Store reference
-      // onBuffer={this.onBuffer} // Callback when remote video is buffering
-      // onError={this.videoError} // Callback when video cannot be loaded
-      style={styles.backgroundVideo}
-    />
-    </View>
+    <View style={styles.container}></View>
+
   );
 };
 
@@ -32,13 +22,7 @@ const styles = StyleSheet.create({
     // backgroundColor: 'red',
     // borderWidth: 1
   },
-  backgroundVideo: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
+
 });
 
 export default Kid;
