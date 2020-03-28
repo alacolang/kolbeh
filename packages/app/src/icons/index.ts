337d0cd5343c208963d94f9b21528e66bf7d5ddf
Yasser Zadeh <yassermzh.germany@gmail.com>
2020-03-28 18:18:24 +0100

    add content: images and videos

--- packages/app/src/icons/index.ts
+++ packages/app/src/icons/index.ts
@@ -4,7 +4,7 @@ import KidIcon from "./images/kid.png";
 import KidActiveIcon from "./images/kid-active.png";
 import HomeIcon from "./images/home.png";
 import HomeActiveIcon from "./images/home-active.png";
-import BackIcon from "./images/back.png";
+import BackIcon from "./images/back3.png";
 import ClipIcon from "./images/clip-active.png";
 import LogoIcon from "./images/logo.png";
 import AnxietyIcon from "./images/empathy.png";
@@ -20,6 +20,16 @@ import AdhdIcon from "./images/adhd.png";
 import AutismIcon from "./images/autism.png";
 import BipolarIcon from "./images/bipolar.png";
 import ParentingIcon from "./images/parenting.png";
+import EmpathyActiveIcon from './images/empathy-active.png'
+import GriefActiveIcon from './images/grief-active.png'
+import StressActiveIcon from './images/stress-active.png'
+import DepressionActiveIcon from './images/depression-active.png'
+import ObsessionActiveIcon from './images/obsession-active.png'
+import EducationActiveIcon from './images/education-active.png'
+import AdhdActiveIcon from './images/adhd-active.png'
+import AutismActiveIcon from './images/autism-active.png'
+import BipolarActiveIcon from './images/bipolar-active.png'
+import ParentingActiveIcon from './images/parenting-active.png'
 
 type Icons = Record<string, ImageSourcePropType>;
 
@@ -45,6 +55,16 @@ const icons: Icons = {
   autism: AutismIcon,
   bipolar: BipolarIcon,
   parenting: ParentingIcon,
+  empathyActive: EmpathyActiveIcon,
+  griefActive: GriefActiveIcon,
+  stressActive: StressActiveIcon,
+  depressionActive: DepressionActiveIcon,
+  obsessionActive: ObsessionActiveIcon,
+  educationActive: EducationActiveIcon,
+  adhdActive: AdhdActiveIcon,
+  autismActive: AutismActiveIcon,
+  bipolarActive: BipolarActiveIcon,
+  parentingActive: ParentingActiveIcon,
 };
 
 export default icons;
