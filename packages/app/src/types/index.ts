2c9db738165a3a7347d7694b632d2295cc7b3161
Yasser Zadeh <yassermzh.germany@gmail.com>
2020-03-28 18:22:40 +0100

    app: change typing

--- packages/app/src/types/index.ts
+++ packages/app/src/types/index.ts
@@ -2,9 +2,15 @@ type Image = {
   url: string;
 };
 
+type Video = {
+  url: string;
+  cover: string;
+};
+
 export type IPost = {
   id: string;
   title: string;
   category: string;
   images: Image[];
+  videos: Video[];
 };
