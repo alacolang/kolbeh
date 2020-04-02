41451ceff0935908b33d1a331f4edb2968cd20de
Yasser Zadeh <yassermzh@gmail.com>
2020-04-02 18:49:47 +0200

    server: deploy with pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'scripts/run.js',
+    // args: 'one',
+    cwd: './packages/server',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    interpreter: 'babel-node',
+    // max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'www',
+      host : 'vps',
+      ref  : 'origin/master',
+      repo : 'git@github.com:yassermzh/yara.git',
+      path : '/home/www/yara-pm2',
+      'post-deploy' : 'yarn && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
