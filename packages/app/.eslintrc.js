51bc564969cf7c4cb0acd68b9623c1cca329b64b
Yasser Zadeh <yassermzh@gmail.com>
2020-04-10 17:07:40 +0200

    app: change eslint rules

--- packages/app/.eslintrc.js
+++ packages/app/.eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: "@react-native-community",
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-console": "error",
+    quotes: ["error", "double"],
+  },
 };
