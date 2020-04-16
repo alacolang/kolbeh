ad380f3b7fe979a01bf5e645c1e9910c01e4b147
Yasser Zadeh <yassermzh@gmail.com>
2020-04-16 23:12:37 +0200

    server: set staging

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,61 @@
 module.exports = {
-  apps : [{
-    name: 'API',
-    script: 'scripts/run.js',
-    // args: 'one',
-    cwd: './packages/server',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    interpreter: 'babel-node',
-    // max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
+  apps: [
+    {
+      name: "API",
+      script: "yarn start",
+      // args: 'one',
+      cwd: "./packages/server",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      interpreter: "babel-node",
+      // max_memory_restart: '1G',
+      env: {
+        PORT: 9000,
+        NODE_ENV: "staging",
+      },
+      env_production: {
+        PORT: 8000,
+        NODE_ENV: "production",
+      },
     },
-    env_production: {
-      NODE_ENV: 'production'
+    {
+      name: "IMAGE",
+      script: "yarn start",
+      cwd: "./packages/image",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      interpreter: "babel-node",
+      env_staging: {
+        PORT: 9010,
+        NODE_ENV: "staging",
+      },
+      env_production: {
+        PORT: 8010,
+        NODE_ENV: "production",
+      },
     }
-  }],
+  ],
 
-  deploy : {
-    production : {
-      user : 'www',
-      host : 'vps',
-      ref  : 'origin/master',
-      repo : 'git@github.com:yassermzh/yara.git',
-      path : '/home/www/yara-pm2',
-      'post-deploy' : 'yarn && yarn workspace server optimize-images && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  deploy: {
+    production: {
+      user: "www",
+      host: "vps",
+      ref: "origin/master",
+      repo: "git@github.com:yassermzh/yara.git",
+      path: "/home/www/yara-staging",
+      "post-deploy":
+        "yarn && yarn workspace server optimize-images && pm2 reload ecosystem.config.js --env production",
+    },
+    staging: {
+      user: "www",
+      host: "vps",
+      ref: `origin/${process.env.BRANCH}`,
+      repo: "git@github.com:yassermzh/yara.git",
+      path: "/home/www/yara-staging",
+      "post-deploy":
+        "yarn && pm2 reload ecosystem.config.js --env staging",
+    },
+  },
 };
