63d885b4b00451b9008ea17ccf45c1b35bf6478e
Yasser Zadeh <yassermzh@gmail.com>
2020-04-17 18:57:26 +0200

    app: rename home page to welcome

--- packages/app/src/navigation/home-stack-navigator.tsx
+++ packages/app/src/navigation/home-stack-navigator.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Home from "../pages/home";
+import Home from "../pages/welcome";
 import Contact from "../pages/contact";
 
 export type HomeStackParamList = {
