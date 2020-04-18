84a272452034adc02a85d6d2770b2c92bee2b941
Yasser Zadeh <yassermzh@gmail.com>
2020-04-19 00:30:27 +0200

    app: update development config

--- packages/app/src/config/development.ts
+++ packages/app/src/config/development.ts
@@ -1,10 +1,10 @@
 import { Config } from "./index";
 
 const config: Config = {
-  // HOST: "http://192.168.43.46:8000",
-  // API: "http://192.168.43.46:8000/graphql",
-  HOST: "http://stg.alacolang.ir/kolbeh",
-  API: "http://stg.alacolang.ir/kolbeh/graphql",
+  HOST: "http://192.168.43.46:8000",
+  API: "http://192.168.43.46:8000/graphql",
+  // HOST: "http://stg.alacolang.ir/kolbeh",
+  // API: "http://stg.alacolang.ir/kolbeh/graphql",
 };
 
 module.exports = config;
