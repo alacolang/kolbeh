a63d8d9605a1a78b22418fbfb9f45a27d6445613
Yasser Zadeh <yassermzh@gmail.com>
2020-04-18 18:31:24 +0200

    server: remove unused code

--- packages/server/src/resolvers/index.js
+++ packages/server/src/resolvers/index.js
@@ -12,68 +12,6 @@ async function init() {
 init();
 
 const dataResolver = (data) => {
-  return data.map((categoryData, i) => {
-    return {
-      ...categoryData,
-      id: categoryData.name + i,
-      icon: categoryData.name,
-      feed: {
-        pageInfo: {
-          hasNextPage: false,
-        },
-        edges: categoryData.feed.map((post, i) => {
-          let videos = [];
-          let images = [];
-          let markdown = null;
-
-          const items = Array.isArray(post) ? post : [post];
-          if (/\.mp4$/.test(items[0])) {
-            videos = items.map((item, j) => ({
-              id: categoryData.name + i + j,
-              url: `/static/videos/${item}`,
-              cover: `/static/images/${item.replace(".mp4", "-cover.webp")}`,
-            }));
-          } else if (/\.md$/.test(items[0])) {
-            const postFile = path.join(__dirname, "./data", items[0]);
-            markdown = {
-              // TODO: add cache
-              content: fs.readFileSync(postFile, "utf8"),
-              cover: `/static/images/${items[0].replace(
-                ".md",
-                "-co ver.webp"
-              )}`,
-            };
-            images = [
-              {
-                id: "age-coping-id",
-                url: `/static/images/age-coping-cover.webp`,
-              },
-            ];
-          } else {
-            images = items.map((item, j) => ({
-              id: categoryData.name + i + j,
-              url: `/static/images/${item}`,
-            }));
-          }
-
-          return {
-            node: {
-              id: `id-${categoryData.name}-${items[0]}`,
-              category: categoryData.name,
-              title: "a-title",
-              description: "the-description",
-              images,
-              videos,
-              markdown,
-            },
-          };
-        }),
-      },
-    };
-  });
-};
-
-const dataResolver2 = (data) => {
   return data.map((d) => {
     return {
       ...d,
@@ -104,12 +42,10 @@ const resolvers = {
         }).node;
     },
     parentCategories: () => {
-      // console.log(JSON.stringify(dataResolver2(parsedData.parent), null, 2));
-      // return dataResolver(data.parentData);
-      return dataResolver2(parsedData.parent);
+      return dataResolver(parsedData.parent);
     },
     childCategories: () => {
-      return dataResolver(data.childData);
+      return dataResolver(parsedData.child);
     },
     ...depricatedResolvers,
   },
