ea16a928f9c74e3491d083d59bbe121f03786e81
Yasser Zadeh <yassermzh@gmail.com>
2020-04-27 02:03:35 +0200

    app: clean up main stack

--- packages/app/src/navigation/main-tab-navigator.tsx
+++ packages/app/src/navigation/main-tab-navigator.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import {
-  View,
-  StyleSheet,
-  Dimensions,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+import { View, StyleSheet, Dimensions, TouchableOpacity } from "react-native";
 import icons from "../components/icon";
 import colors from "../colors";
 import { FormattedText } from "../components/formatted-text";
@@ -14,6 +8,7 @@ import ParentNavigator from "./parent-stack-navigator";
 import ChildNavigator from "./child-stack-navigator";
 import HomeNavigator from "./home-stack-navigator";
 import Curve from "../components/curve";
+import { Icon } from "../components/icon";
 
 export type TabParamList = {
   home: undefined;
@@ -25,7 +20,7 @@ const Tab = createBottomTabNavigator<TabParamList>();
 
 const fullWidth = Dimensions.get("window").width;
 
-const TabBar = ({ state, descriptors, navigation }) => {
+const TabBar = ({ state, navigation }) => {
   return (
     <View
       style={{
@@ -53,22 +48,12 @@ const TabBar = ({ state, descriptors, navigation }) => {
         }}
       >
         {state.routes.map((route, index) => {
-          const { options } = descriptors[route.key];
-          const label =
-            options.tabBarLabel !== undefined
-              ? options.tabBarLabel
-              : options.title !== undefined
-              ? options.title
-              : route.name;
-
           const focused = state.index === index;
-          const icon = icons[route.name + (focused ? "Active" : "")];
           return (
             <TouchableOpacity onPress={() => navigation.navigate(route.name)}>
               <View
                 style={{
                   borderWidth: 0,
-                  // elevation: 1,
                   height: 50,
                   width: 50,
                   borderRadius: 50,
@@ -76,8 +61,10 @@ const TabBar = ({ state, descriptors, navigation }) => {
                   alignItems: "center",
                 }}
               >
-                <Image source={icon} style={styles.icon} resizeMode="contain" />
-                {/* <FormattedText style={[styles.text]} id={route.name} /> */}
+                <Icon
+                  name={route.name + (focused ? "Active" : "")}
+                  size="small"
+                />
                 <FormattedText
                   style={[
                     styles.text,
@@ -137,7 +124,6 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 12,
   },
-  icon: { width: 30, height: 30 },
 });
 
 export default TabNavigator;
