7b4916a94d2b23480aa84da7f4e24e682c42c8a9
Yasser Zadeh <yassermzh@gmail.com>
2020-04-29 12:15:56 +0200

    app: change host for ios/android in dev mode

--- packages/app/src/config/development.ts
+++ packages/app/src/config/development.ts
@@ -1,8 +1,13 @@
 import { Config } from "./index";
+import { Platform } from "react-native";
+
+const isIOS = Platform.OS === "ios";
 
 const config: Config = {
-  HOST: "http://192.168.43.46:8000",
-  API: "http://192.168.43.46:8000/graphql",
+  HOST: isIOS ? "http://localhost:8000" : "http://192.168.43.179:8000",
+  API: isIOS
+    ? "http://localhost:8000/graphql"
+    : "http://192.168.43.179:8000/graphql",
   // HOST: "http://stg.alacolang.ir/kolbeh",
   // API: "http://stg.alacolang.ir/kolbeh/graphql",
 };
