93eee4bda68b75df801607c233bb5481b904ad93
Yasser Zadeh <yassermzh.germany@gmail.com>
2020-05-08 14:59:44 +0200

    app: upgrade to 0.62 android

--- packages/app/android/app/src/main/java/ir/alacolang/kolbeh/MainApplication.java
+++ packages/app/android/app/src/main/java/ir/alacolang/kolbeh/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
+import com.facebook.react.ReactInstanceManager;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.facebook.react.ReactNativeHost;
@@ -56,23 +58,29 @@ public class MainApplication extends Application implements ReactApplication {
       sharedI18nUtilInstance.forceRTL(this, true); //<== AmerllicA config
       sharedI18nUtilInstance.allowRTL(this, true); //<== AmerllicA config
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
   /**
    * Loads Flipper in React Native templates.
+   * Call this in the onCreate method with something like
+   *     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
    *
    * @param context
+   * @param reactInstanceManager
    */
-  private static void initializeFlipper(Context context) {
-    if (BuildConfig.DEBUG) {
+  private static void initializeFlipper(
+          Context context, ReactInstanceManager reactInstanceManager) {
+      if (BuildConfig.DEBUG) {
       try {
         /*
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
-        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+          Class<?> aClass = Class.forName("ir.alacolang.kolbeh.ReactNativeFlipper");
+          aClass
+                  .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+                  .invoke(null, context, reactInstanceManager);
       } catch (ClassNotFoundException e) {
         e.printStackTrace();
       } catch (NoSuchMethodException e) {
