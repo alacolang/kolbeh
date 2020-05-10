f8f5d354d0b9cb87871c8d51585cbaad5304a10b
Yasser Zadeh <yassermzh.germany@gmail.com>
2020-05-10 12:01:41 +0200

    app: revert navigators to normal

--- packages/app/src/navigation/splash-stack-navigator.tsx
+++ packages/app/src/navigation/splash-stack-navigator.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import MainNavigator from "./main-tab-navigator";
 import SplashScreen from "../pages/splash";
-import BodyPercussionScreen from "../pages/body-percussion";
 
 export type StackParamList = {
   splash: undefined;
@@ -12,7 +11,7 @@ export type StackParamList = {
 
 const Stack = createStackNavigator<StackParamList>();
 const SplashNavigator = () => (
-  <Stack.Navigator initialRouteName="bodyPercussion">
+  <Stack.Navigator initialRouteName="splash">
     <Stack.Screen
       name="splash"
       component={SplashScreen}
@@ -21,11 +20,6 @@ const SplashNavigator = () => (
     <Stack.Screen
       name="main"
       component={MainNavigator}
-      options={{ header: () => null }}
-    />
-    <Stack.Screen
-      name="bodyPercussion"
-      component={BodyPercussionScreen}
       options={{ header: () => null }}
     />
   </Stack.Navigator>
