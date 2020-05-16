e60bcc082704c6add9a186bdf4650f860462bd99
Yasser Zadeh <yassermzh@gmail.com>
2020-05-17 00:12:32 +0200

    app: fixed typo

--- packages/app/src/navigation/index.tsx
+++ packages/app/src/navigation/index.tsx
@@ -2,13 +2,13 @@ import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import MainTabNavigator from "./main-tab-navigator";
-import SplahNavigator from "./splash-stack-navigator";
+import SplashNavigator from "./splash-stack-navigator";
 import ChildNavigator from "./child-stack-navigator";
 
 const createAppContainer = () => {
   return (
     <NavigationContainer>
-      <SplahNavigator />
+      <SplashNavigator />
       {/* <MainTabNavigator /> */}
       {/* <ChildNavigator /> */}
     </NavigationContainer>
