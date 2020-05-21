c345a7e16b068db9393d6b0f467cc2b5a0530ff7
Yasser Zadeh <yassermzh@gmail.com>
2020-05-22 00:41:57 +0200

    server: create thumbnails for progressive images

--- packages/server/scripts/optimize-images.js
+++ packages/server/scripts/optimize-images.js
@@ -1,28 +1,43 @@
+const pLimit = require("p-limit");
 const sharp = require("sharp");
 const glob = require("glob");
 // const fs = require("fs-extra");
 const matches = glob.sync("src/static/images/**/*.{png,jpg,jpeg}");
-// const MAX_WIDTH = 1800;
+// const matches = [
+//   'src/static/images/masturbation-cover.png',
+//   'src/static/images/parastooha-chap2-1.png'
+// ]
+const MAX_WIDTH_LOW = 150;
 // const QUALITY = 85;
+
+const limit = pLimit(5);
 
 const newExt = ".webp";
 
-Promise.all(
-  matches.map(async (match) => {
-    const stream = sharp(match);
-    // const info = await stream.metadata();
-    // if (info.width < MAX_WIDTH) {
-    //   return;
-    // }
-    const optimizedName = match.replace(
-      /(\..+)$/,
-      (match, ext) => `${newExt}`
-    );
-    await stream
-      // .resize(MAX_WIDTH)
-      .webp()
-      .toFile(optimizedName);
+async function doProcess() {
+  console.log("optimizing images...");
+  await Promise.all(
+    matches.slice(0, 10).map(async (match) => {
+      const stream = sharp(match);
+      // const info = await stream.metadata();
+      // if (info.width < MAX_WIDTH) {
+      //   return;
+      // }
+      const optimizedName = match.replace(
+        /(\..+)$/,
+        (match, ext) => `${newExt}`
+      );
+      const lowName = match.replace(
+        /(\..+)$/,
+        (match, ext) => `-${MAX_WIDTH_LOW}${newExt}`
+      );
+      limit(() => stream.webp().toFile(optimizedName));
+      limit(() => stream.resize(MAX_WIDTH_LOW).webp().toFile(lowName));
+      // return fs.rename(optimizedName, newName);
+    })
+  );
 
-    // return fs.rename(optimizedName, newName);
-  })
-);
+  console.log("optimizing images...done");
+}
+
+doProcess();
