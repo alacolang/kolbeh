4d2bb7a642d8d35e2cf828f791e1b50f85268b72
Yasser Zadeh <yassermzh@gmail.com>
2020-09-20 16:38:29 +0200

    app: add some icon images

--- packages/app/src/components/icon/images/index.ts
+++ packages/app/src/components/icon/images/index.ts
@@ -1,94 +1,105 @@
-import ParentActiveIcon from "./parent-active.png";
-import ParentIcon from "./parent.png";
-import ChildIcon from "./child.png";
-import ChildActiveIcon from "./child-active.png";
-import HomeIcon from "./home.png";
-import HomeActiveIcon from "./home-active.png";
-import BackIcon from "./back.png";
-import BackDarkIcon from "./back-dark.png";
-import LogoIcon from "./logo.png";
-import EmpathyActiveIcon from "./empathy-active.png";
-import GriefActiveIcon from "./grief-active.png";
-import StressActiveIcon from "./stress-active.png";
-import DepressionActiveIcon from "./depression-active.png";
-import ObsessionActiveIcon from "./obsession-active.png";
-import AnxietyActiveIcon from "./anxiety.png";
-import EducationActiveIcon from "./education-active.png";
-import AdhdActiveIcon from "./adhd-active.png";
-import AutismActiveIcon from "./autism-active.png";
-import BipolarActiveIcon from "./bipolar-active.png";
-import ParentingActiveIcon from "./parenting-active.png";
-import KidIcon from "./kid.png";
-import TeenIcon from "./teen.png";
-import KidActiveIcon from "./kid-active.png";
-import TeenActiveIcon from "./teen-active.png";
-import InstagramIcon from "./instagram.png";
-import TwitterIcon from "./twitter.png";
-import TelegramIcon from "./telegram.png";
-import InfoIcon from "./information.png";
-import VideoIcon from "./video.png";
-import LoadingIcon from "./loading.png";
-import SaveActiveIcon from "./save-active.png";
-import SaveIcon from "./save.png";
-import SavedIcon from "./saved.png";
-import ShareActiveIcon from "./share-active.png";
-import ToolboxIcon from "./toolbox.png";
-import LoadingVarientIcon from "./loading-light.png";
-import SearchIcon from "./search.png";
-import LeftArrowIcon from "./left-arrow.png";
-import TimesIcon from "./times.png";
-import PauseIcon from "./pause.png";
-import PlayIcon from "./play.png";
-import ShopIcon from "./shop.png";
-import ProfileIcon from './profile.png'
-import CheckIcon from './check.png'
+import parentActive from "./parent-active.png";
+import parent from "./parent.png";
+import child from "./child.png";
+import childActive from "./child-active.png";
+import home from "./home.png";
+import homeActive from "./home-active.png";
+import back from "./back.png";
+import backDark from "./back-dark.png";
+import logo from "./logo.png";
+import empathy from "./empathy.png";
+import grief from "./grief.png";
+import stress from "./stress.png";
+import depression from "./depression.png";
+import obsession from "./obsession.png";
+import anxiety from "./anxiety.png";
+import education from "./education.png";
+import adhd from "./adhd.png";
+import autism from "./autism.png";
+import bipolar from "./bipolar.png";
+import parenting from "./parenting.png";
+import kid from "./kid.png";
+import teen from "./teen.png";
+import kidActive from "./kid-active.png";
+import teenActive from "./teen-active.png";
+import instagram from "./instagram.png";
+import twitter from "./twitter.png";
+import telegram from "./telegram.png";
+import info from "./information.png";
+import video from "./video.png";
+import loading from "./loading.png";
+import bookmarkActive from "./bookmark-active.png";
+import bookmark from "./bookmark.png";
+// import saved from "./saved.png";
+import shareActive from "./share-active.png";
+import toolbox from "./toolbox.png";
+import loadingVarient from "./loading-light.png";
+import search from "./search.png";
+import leftArrow from "./left-arrow.png";
+import times from "./times.png";
+import pause from "./pause.png";
+import play from "./play.png";
+import shop from "./shop.png";
+import profile from "./profile.png";
+import check from "./check.png";
+import kolbehActive from "./kolbeh-active.png";
+import kolbeh from "./kolbeh.png";
+import circle from "./circle.png";
+import checkCircle from "./check-circle.png";
+import lockCircle from "./lock-circle.png";
+import tickActive from "./tick-active.png";
 
 const icons = {
-  parent: ParentIcon,
-  parentActive: ParentActiveIcon,
-  child: ChildIcon,
-  childActive: ChildActiveIcon,
-  home: HomeIcon,
-  homeActive: HomeActiveIcon,
-  back: BackIcon,
-  backDark: BackDarkIcon,
-  logo: LogoIcon,
-  empathyActive: EmpathyActiveIcon,
-  griefActive: GriefActiveIcon,
-  stressActive: StressActiveIcon,
-  // todo: typo in anxiety!
-  axietyActive: AnxietyActiveIcon,
-  depressionActive: DepressionActiveIcon,
-  obsessionActive: ObsessionActiveIcon,
-  educationActive: EducationActiveIcon,
-  adhdActive: AdhdActiveIcon,
-  autismActive: AutismActiveIcon,
-  bipolarActive: BipolarActiveIcon,
-  parentingActive: ParentingActiveIcon,
-  kid: KidIcon,
-  teen: TeenIcon,
-  kidActive: KidActiveIcon,
-  teenActive: TeenActiveIcon,
-  instagram: InstagramIcon,
-  twitter: TwitterIcon,
-  telegram: TelegramIcon,
-  info: InfoIcon,
-  video: VideoIcon,
-  loading: LoadingIcon,
-  loadingVarient: LoadingVarientIcon,
-  saveActive: SaveActiveIcon,
-  save: SaveIcon,
-  saved: SavedIcon,
-  shareActive: ShareActiveIcon,
-  toolbox: ToolboxIcon,
-  search: SearchIcon,
-  leftArrow: LeftArrowIcon,
-  times: TimesIcon,
-  pause: PauseIcon,
-  play: PlayIcon,
-  shop: ShopIcon,
-  profile: ProfileIcon,
-  check: CheckIcon
+  parent,
+  parentActive,
+  child,
+  childActive,
+  home,
+  homeActive,
+  back,
+  backDark,
+  logo,
+  empathy,
+  grief,
+  stress,
+  axiety: anxiety,
+  depression,
+  obsession,
+  education,
+  adhd,
+  autism,
+  bipolar,
+  parenting,
+  kid,
+  teen,
+  kidActive,
+  teenActive,
+  instagram,
+  twitter,
+  telegram,
+  info,
+  video,
+  loading,
+  loadingVarient,
+  bookmarkActive,
+  bookmark,
+  // saved,
+  shareActive,
+  toolbox,
+  search,
+  leftArrow,
+  times,
+  pause,
+  play,
+  shop,
+  profile,
+  check,
+  kolbeh,
+  kolbehActive,
+  checkCircle,
+  lockCircle,
+  circle,
+  tickActive,
 };
 
 export default icons;
---
+++ packages/app/src/components/icon/images/svgs.ts
@@ -0,0 +1,9 @@
+import tickFill from "./tick-fill.svg";
+import tickOutline from "./tick-outline.svg";
+import cloud from "./cloud.svg";
+
+export default {
+  tickFill,
+  tickOutline,
+  cloud,
+};
