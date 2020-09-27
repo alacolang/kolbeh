394fdd720370b5def5252b6b074845d21feb84a6
Yasser Zadeh <yassermzh@gmail.com>
2020-09-28 00:15:22 +0200

    app: setup jest

---
+++ packages/app/jest/setup.js
@@ -0,0 +1,45 @@
+import "react-native-gesture-handler/jestSetup";
+import mockAsyncStorage from "@react-native-community/async-storage/jest/async-storage-mock";
+import fetchMock from "jest-fetch-mock";
+
+jest.mock("react-native-reanimated", () => {
+  const Reanimated = require("react-native-reanimated/mock");
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock("react-native/Libraries/Animated/src/NativeAnimatedHelper");
+
+jest.mock("@react-native-community/async-storage", () => mockAsyncStorage);
+
+jest.mock("react-native-sound", () => {});
+
+jest.mock("react-native-code-push", () => {
+  const cp = (_) => (app) => app;
+  Object.assign(cp, {
+    InstallMode: {},
+    CheckFrequency: {},
+    SyncStatus: {},
+    UpdateState: {},
+    DeploymentStatus: {},
+    DEFAULT_UPDATE_DIALOG: {},
+
+    checkForUpdate: jest.fn(),
+    codePushify: jest.fn(),
+    getConfiguration: jest.fn(),
+    getCurrentPackage: jest.fn(),
+    getUpdateMetadata: jest.fn(),
+    log: jest.fn(),
+    notifyAppReady: jest.fn(),
+    notifyApplicationReady: jest.fn(),
+    sync: jest.fn(),
+  });
+  return cp;
+});
+
+fetchMock.enableMocks();
