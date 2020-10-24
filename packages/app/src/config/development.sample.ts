b87b4264e97ad823a5cb4c6b12947e0dda37187c
Yasser Zadeh <yassermzh@gmail.com>
2020-10-24 17:40:46 +0200

    app: change config setup

--- packages/app/src/config/development.sample.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from "./index";
-
-const config: Config = {
-  HOST: "http://localhost:8000",
-  API: "http://localhost:8000/graphql",
-};
-
-module.exports = config;
--- packages/app/src/config/index.ts
+++ packages/app/src/config/index.ts
@@ -1,11 +1,34 @@
+import { Platform } from "react-native";
+import RNConfig from "react-native-config";
+
 export type Config = {
   HOST: string;
   API: string;
+  ANALYTICS_DISABLED?: boolean;
+  isDevelopment: boolean;
 };
 
-let config: Config =
-  process.env.NODE_ENV === "production"
-    ? require("./production")
-    : require("./development");
+const isAndroid4 = () => {
+  return Platform.Version < 21;
+};
+const getSchema = () => {
+  return isAndroid4() ? "http" : "https";
+};
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+function fixSchema(uri: string): string {
+  if (isDevelopment) {
+    return uri;
+  }
+  return uri.replace(/http(s?)/i, getSchema());
+}
+
+const config: Config = {
+  HOST: fixSchema(RNConfig.HOST),
+  API: fixSchema(RNConfig.API),
+  ANALYTICS_DISABLED: RNConfig.ANALYTICS_DISABLED === "true",
+  isDevelopment,
+};
 
 export default config;
