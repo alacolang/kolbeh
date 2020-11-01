1ecb239c1390f0bb97c62e472df716f70615c00a
Yasser Zadeh <yassermzh@gmail.com>
2020-11-01 21:45:28 +0100

    app: update images

--- packages/app/src/pages/onboarding/index.tsx
+++ packages/app/src/pages/onboarding/index.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Swiper from "react-native-swiper";
-import { View, Image, StyleSheet, Dimensions, StatusBar } from "react-native";
-import img1 from "../../assets/images/onboarding-1.png";
-import img2 from "../../assets/images/onboarding-2.png";
-import img3 from "../../assets/images/onboarding-3.png";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Dimensions,
+  StatusBar,
+  ImageSourcePropType,
+} from "react-native";
+import img1 from "../../assets/images/1.gif";
+import img2 from "../../assets/images/2.gif";
+import img3 from "../../assets/images/3.gif";
+import img from "../../assets/images/onboarding-image.png";
 import { FormattedText } from "components/formatted-text";
 import colors from "../../colors";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { IconSvg } from "components/icon";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Trans, useTranslation } from "react-i18next";
 import { StackScreenProps } from "@react-navigation/stack";
@@ -15,29 +22,52 @@ import { HomeStackParamList } from "navigation/home-stack-navigator";
 
 const frameWidth = Dimensions.get("window").width;
 const frameHeight = Dimensions.get("window").height;
-
-const ImageHeight = frameHeight / 2.2;
+const imageSize = Math.min(frameWidth - 40 * 2, frameHeight / 2.5);
 
 type Props = StackScreenProps<HomeStackParamList, "onboarding">;
 const Onboarding = ({ navigation }: Props) => {
   const { t } = useTranslation();
   return (
-    <SafeAreaView style={styles.container} edges={["top", "bottom"]}>
+    <SafeAreaView style={styles.container} edges={["top"]}>
       <StatusBar hidden />
 
       <Swiper
-        showsButtons={false}
+        showsButtons={true}
         loop={false}
-        activeDotColor={colors.secondary}
-        dotColor={colors.primary}
-        paginationStyle={{ flexDirection: "row-reverse" }}
+        activeDotColor={colors.backgroundSecondary}
+        dotColor={colors.backgroundPrimaryThird}
+        paginationStyle={{
+          flexDirection: "row-reverse",
+          top: imageSize + 36 + 36 + 16,
+          // bottom: 0,
+          alignItems: "flex-start",
+          // borderWidth: 1,
+        }}
+        buttonWrapperStyle={{
+          position: "absolute",
+          paddingBottom: 24,
+          justifyContent: "center",
+          alignItems: "flex-end",
+          backgroundColor: "transparent",
+        }}
+        prevButton={<View />}
+        nextButton={
+          <Action
+            text={t("onboarding.next")}
+            onPress={() => {
+              navigation.navigate("home");
+            }}
+          />
+        }
       >
         <View style={styles.slide}>
+          <Gif image={img3} />
           <Title text={t("onboarding.3.title")} />
           <Description id={t("onboarding.3.description")} />
-          <Image source={img3} style={styles.image} resizeMode="contain" />
+
           <View style={styles.doneContainer}>
-            <DoneButton
+            <Action
+              text={t("onboarding.done")}
               onPress={() => {
                 navigation.navigate("home");
               }}
@@ -45,15 +75,15 @@ const Onboarding = ({ navigation }: Props) => {
           </View>
         </View>
         <View style={styles.slide}>
+          <Gif image={img2} />
           <Title text={t("onboarding.2.title")} />
           <Description id={t("onboarding.2.description")} />
-          <Image source={img2} style={styles.image} resizeMode="contain" />
         </View>
 
         <View style={styles.slide}>
+          <Gif image={img1} />
           <Title text={t("onboarding.1.title")} />
           <Description id={t("onboarding.1.description")} />
-          <Image source={img1} style={styles.image} resizeMode="contain" />
         </View>
       </Swiper>
     </SafeAreaView>
@@ -62,12 +92,6 @@ const Onboarding = ({ navigation }: Props) => {
 
 const Description = ({ id }: { id: string }) => (
   <View style={descriptionStyles.headerContainer}>
-    <IconSvg
-      name="cloud"
-      color="white"
-      size={40}
-      style={descriptionStyles.cloud1}
-    />
     <FormattedText style={descriptionStyles.text}>
       <Trans
         i18nKey={id}
@@ -87,77 +111,117 @@ const descriptionStyles = StyleSheet.create({
     alignItems: "center",
     width: "100%",
   },
-  cloud1: { position: "absolute", bottom: 16, right: 16 },
   text: {
-    marginTop: 30,
-    marginBottom: 30,
-    height: 120,
+    textAlign: "center",
+    marginTop: 16,
+    // height: 120,
     lineHeight: 24 * 1.6,
-    color: colors.primary,
+    color: colors.secondary,
     fontSize: 23,
     // borderWidth: 1,
   },
 });
 
+const Gif = ({ image }: { image: ImageSourcePropType }) => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setShow(true), 1000);
+  }, []);
+  return (
+    <View style={gifStyles.imageContainer}>
+      <Image source={img} style={gifStyles.imageOverlay} resizeMode="contain" />
+      {show && <Image source={image} style={gifStyles.image} />}
+      {/* <IconSvg
+      name="cloud"
+      color={colors[11]}
+      size={40}
+      style={gifStyles.cloud1}
+    />
+    <IconSvg
+      name="cloud"
+      color={colors.lightBlue}
+      size={80}
+      style={gifStyles.cloud2}
+    /> */}
+    </View>
+  );
+};
+
+const gifStyles = StyleSheet.create({
+  cloud1: { position: "absolute", bottom: imageSize / 2, right: -24 },
+  cloud2: {
+    position: "absolute",
+    top: 20,
+    left: -24,
+  },
+  imageContainer: {
+    height: imageSize + 20,
+    width: imageSize + 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  overlay: {
+    backgroundColor: colors.backgroundPrimary,
+  },
+  imageOverlay: {
+    position: "absolute",
+    height: imageSize + 40,
+    width: imageSize + 40,
+  },
+  image: {
+    height: imageSize * 0.6,
+    width: imageSize * 0.6,
+  },
+});
+
 const Title = ({ text }: { text: string }) => (
   <View style={titleStyles.headerContainer}>
-    <IconSvg name="cloud" color="white" size={65} style={titleStyles.cloud1} />
     <FormattedText style={titleStyles.text}>{text}</FormattedText>
   </View>
 );
 
 const titleStyles = StyleSheet.create({
   headerContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
+    marginTop: 36 + 36,
   },
-  cloud1: { position: "absolute", bottom: 0, left: 16 },
   text: {
+    textAlign: "center",
     color: colors.primary,
     fontSize: 32,
   },
 });
 
-type DoneButtonProps = { onPress: () => void };
-function DoneButton({ onPress }: DoneButtonProps) {
+type ActionProps = { onPress: () => void; text: string };
+function Action({ onPress, text }: ActionProps) {
   return (
-    <TouchableOpacity
-      onPress={() => onPress()}
-      style={doneButtonStyles.container}
-    >
-      <View style={doneButtonStyles.iconContainer}>
-        <IconSvg name="gaussCurve" size={90} color={colors.secondary} />
-      </View>
-      <View style={doneButtonStyles.icon2Container}>
-        <IconSvg name="tickFill" size={26} color="white" />
-      </View>
+    <TouchableOpacity onPress={() => onPress()} style={actionStyles.container}>
+      <FormattedText style={actionStyles.text}>{text}</FormattedText>
     </TouchableOpacity>
   );
 }
-const doneButtonStyles = StyleSheet.create({
+const actionStyles = StyleSheet.create({
   container: {
-    height: 90,
     width: 60,
+    height: 60,
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 7,
+    borderColor: "rgba(255,255,255,.5)",
+    backgroundColor: colors.backgroundPrimary,
+    elevation: 5,
   },
-  iconContainer: {
-    position: "absolute",
-    borderColor: "green",
-    right: -7,
-  },
-  icon2Container: {
-    zIndex: 2,
-    position: "absolute",
-    top: 32,
-    right: 25,
+  text: {
+    fontSize: 16,
+    top: -3,
+    color: "rgba(255,255,255,.7)",
   },
 });
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginBottom: 16,
-    backgroundColor: colors.backgroundVariant,
+    backgroundColor: colors.backgroundPrimary,
   },
   slide: {
     flex: 1,
@@ -165,11 +229,14 @@ const styles = StyleSheet.create({
     paddingTop: 36,
     alignItems: "center",
   },
-  image: {
-    height: ImageHeight,
-    width: frameWidth - 40,
+  doneContainer: {
+    position: "absolute",
+    bottom: 0,
+    paddingBottom: 24,
+    justifyContent: "center",
+    alignItems: "flex-end",
+    backgroundColor: "transparent",
   },
-  doneContainer: { position: "absolute", left: 0, bottom: 0 },
 });
 
 export default Onboarding;
