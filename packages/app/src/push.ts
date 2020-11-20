b2e10cb616a0eb2d0709372283e5b62dcbe5953a
Yasser Zadeh <yassermzh@gmail.com>
2020-11-20 22:40:20 +0100

    app: cleanup

--- packages/app/src/push.ts
+++ packages/app/src/push.ts
@@ -1,4 +1,5 @@
 import PushNotification from "react-native-push-notification";
+import { log } from "utils/log";
 
 // can't use react-native-firebase to scheduled notifications!
 // so here I use react-native-push-notifications for scheduled notifications
@@ -17,7 +18,7 @@ PushNotification.configure({
 
   // (required) Called when a remote is received or opened, or local notification is opened
   onNotification: function (notification) {
-    console.log("NOTIFICATION:", notification);
+    log("NOTIFICATION:", notification);
 
     // process the notification
 
@@ -27,8 +28,8 @@ PushNotification.configure({
 
   // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
   onAction: function (notification) {
-    console.log("ACTION:", notification.action);
-    console.log("NOTIFICATION:", notification);
+    // log("ACTION:", notification.action);
+    log("NOTIFICATION:", notification);
 
     // process the action
   },
@@ -36,13 +37,6 @@ PushNotification.configure({
   // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
   onRegistrationError: function (err) {
     console.error(err.message, err);
-  },
-
-  // IOS ONLY (optional): default: all - Permissions to register.
-  permissions: {
-    alert: true,
-    badge: true,
-    sound: true,
   },
 
   // Should the initial notification be popped automatically
@@ -67,28 +61,7 @@ PushNotification.createChannel(
     importance: 4, // (optional) default: 4. Int value of the Android notification importance
     vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
   },
-  (created) =>
-    console.log(`createChannel 'happiness-reminder' returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+  (created) => {
+    console.log(`createChannel 'happiness-reminder' returned '${created}'`); // (optional) callback returns whether the channel was created, false means it already existed.
+  }
 );
-
-setTimeout(() => {
-  // PushNotification.localNotification({
-  //   /* Android Only Properties */
-  //   channelId: "happiness-reminder", //
-  //   message: "My Notification Message", // (required)
-  // });
-
-  PushNotification.getChannels(function (channel_ids) {
-    // console.log({ channel_ids }); // ['channel_id_1']
-  });
-
-  PushNotification.channelBlocked("happiness-reminder", function (blocked) {
-    // console.log({ blocked }); // true/false
-  });
-
-  PushNotification.getScheduledLocalNotifications(
-    (scheduledLocalNotifications) => {
-      console.log({ scheduledLocalNotifications });
-    }
-  );
-}, 1000);
