97a68d17ab90a51da41e5d44c308b4650b34eeff
Yasser Zadeh <yassermzh@gmail.com>
2020-11-22 18:13:19 +0100

    ci: fix deploy script

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,7 +1,6 @@
 yarn install
 yarn workspace server build
 yarn workspace server optimize-images
-cd packages/server
-cp ../../../shared/.env .
-cp ../../../shared/firebase.json ./lib/
-cp -r ./src/content ./lib/
+cp ../shared/.env packages/server
+cp ../shared/firebase.json packages/server/lib/
+cp -r packages/server/src/content packages/server/lib/
