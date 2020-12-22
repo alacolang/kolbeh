56ab97db4aaea27a1734ab737c5e1d54a0c1b29d
Soroush Mohammadzadeh <soroushmzy@gmail.com>
2020-12-22 17:50:43 +0330

    ci(server): fix copy command

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,7 +1,8 @@
 yarn install
+cp ../shared/.env packages/server
+cp ../shared/firebase.json packages/server/src/
 yarn workspace server build
 yarn workspace server optimize-images
-cp ../shared/.env packages/server
 cp ../shared/firebase.json packages/server/lib/
 cp -r packages/server/src/content packages/server/lib/
 cp -r packages/server/src/static packages/server/lib/
