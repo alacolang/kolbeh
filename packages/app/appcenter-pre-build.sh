0b161aca7caf89d32adc7ff5d911a78b6cb73024
Yasser Zadeh <yassermzh@gmail.com>
2020-12-23 10:44:56 +0100

    ci: appcenter pre build to copy google services json

---
+++ packages/app/appcenter-pre-build.sh
@@ -0,0 +1,8 @@
+GOOGLE_SERVICES_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/packages/app/android/app/google-services.json
+
+echo "Creating google-services.json"
+echo "$GOOGLE_SERVICES_JSON" > $GOOGLE_SERVICES_JSON_FILE
+sed -i -e 's/\\"/'\"'/g' $GOOGLE_SERVICES_JSON_FILE
+
+echo "File content:"
+cat $GOOGLE_SERVICES_JSON_FILE
