132b87807fc35c665f77c54acc0882f3690a578d
Soroush Mohammadzadeh <sorooshmzy@gmail.com>
2020-12-29 20:26:14 +0330

    feat(app): add loading icon when videos load

--- packages/app/src/pages/post/video-post.tsx
+++ packages/app/src/pages/post/video-post.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import Video from "react-native-video";
 import * as Types from "types";
 import { resolveURL } from "utils/resolve";
+import Loading from "../../components/loading/index";
+import Color from "../../colors";
 
 type Props = {
   post: Types.IPost;
 };
 
+const bufferConfig = {
+  minBufferMs: 5000, //default is 15000
+  maxBufferMs: 50000,
+  bufferForPlaybackMs: 2500,
+  bufferForPlaybackAfterRebufferMs: 5000,
+};
+const minLoadRetryCount = 5;
+
 const VideoPost = ({ post }: Props) => {
   const video = post.videos[0];
   const uri = resolveURL(video.url);
+  const [videoIsLoaded, setVideoIsLoaded] = useState(false);
 
   return (
     <View style={styles.modalContainer}>
@@ -18,15 +29,23 @@ const VideoPost = ({ post }: Props) => {
         source={{ uri }}
         style={styles.backgroundVideo}
         // paused={pause}
-        // onLoad={() => {}}
+        onLoad={() => {
+          setVideoIsLoaded(true);
+        }}
         // onError={() => {}}
+        bufferConfig={bufferConfig}
+        minLoadRetryCount={minLoadRetryCount}
         resizeMode="contain"
-        // minLoadRetryCount={4}
         controls={true}
         poster={video.cover ? resolveURL(video.cover) : undefined}
         // posterResizeMode="cover"
         hideShutterView={true}
       />
+      {videoIsLoaded ? null : (
+        <View style={styles.loadingIcon}>
+          <Loading color={Color.primary} />
+        </View>
+      )}
     </View>
   );
 };
@@ -46,6 +65,11 @@ const styles = StyleSheet.create({
     left: 0,
     bottom: 0,
     right: 0,
+  },
+  loadingIcon: {
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
